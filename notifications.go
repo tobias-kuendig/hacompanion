35cc76199573b64724c20920dd384b4f93f413fb
Tobias Kündig <tobias@offline.ch>
2021-05-15 18:11:59 +0200

    Fixed notify-send call

--- notifications.go
+++ notifications.go
@@ -44,18 +44,24 @@ func (s NotificationServer) Listen(ctx context.Context) {
 			util.RespondError(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+		log.Printf("received notification payload: %+v", req)
 		if req.PushToken != s.registration.PushToken {
+			log.Printf("notification push token is wrong: %s", req.PushToken)
 			util.RespondError(w, "wrong token", http.StatusUnauthorized)
 			return
 		}
 		err = notification.Send(r.Context(), req.Title, req.Message, req.Data)
 		if err != nil {
+			log.Printf("failed to send notification: %s", err)
 			util.RespondError(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		log.Println("notification sent successfully")
 		w.WriteHeader(201)
 		util.RespondSuccess(w)
 	})
+
+	log.Printf("starting notification server on %s (with token %s)", s.address, s.registration.PushToken)
 
 	if err := s.Server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 		log.Fatalf("failed to start notifications server: %s", err)
@@ -73,13 +79,16 @@ func (n *Notification) Send(ctx context.Context, title, message string, data api
 	if data.Urgency != "" {
 		args = append(args, "-u", data.Urgency)
 	}
-	args = append(args, "-a", "Home Assistant")
+//	args = append(args, "-a", "'Home Assistant'")
 	if title != "" {
 		args = append(args, title)
 	}
 	args = append(args, message)
+	log.Printf("comand is: notify-send %v", args)
 	cmd := exec.CommandContext(ctx, "notify-send", args...)
+	cmd.Env = []string{"DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus"}
 	if err := cmd.Run(); err != nil {
+		log.Printf("Return :%+v", err)
 		return err
 	}
 	return nil
