e755a028da4c9d40eca3f7970e512b854fe511b8
Philipp Schmitt <philipp@schmitt.co>
2022-03-19 10:52:27 +0100

    Dynamic icons for custom scripts

--- entity/payload.go
+++ entity/payload.go
@@ -1,6 +1,7 @@
 package entity
 
 type Payload struct {
+	Icon       string                 `json:"icon,omitempty"`
 	State      interface{}            `json:"state,omitempty"`
 	Attributes map[string]interface{} `json:"attributes,omitempty"`
 }
