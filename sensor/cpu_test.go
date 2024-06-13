52409d07edc95806cbb52d14ac36b637d5e20e44
Tobias Kündig <tobias@offline.ch>
2024-06-13 17:21:35 +0200

    Fixed tests

--- sensor/cpu_test.go
+++ sensor/cpu_test.go
@@ -38,7 +38,7 @@ func TestCPUUsage(t *testing.T) {
 	require.EqualValues(t, output, res)
 }
 
-func TestCPUTemp(t *testing.T) {
+func TestCPUTemp_Intel(t *testing.T) {
 	input := `
 		coretemp-isa-0000
 		Adapter: ISA adapter
@@ -57,10 +57,6 @@ func TestCPUTemp(t *testing.T) {
 		pch_cannonlake-virtual-0
 		Adapter: Virtual device
 		temp1:        +37.0°C  
-
-		k10temp-pci-00c3
-		Adapter: PCI adapter
-		Tctl:         +39.6°C
 	`
 	output := &entity.Payload{
 		State: "99.0",
@@ -72,7 +68,30 @@ func TestCPUTemp(t *testing.T) {
 			"core_4": "38.0",
 			"core_5": "39.0",
 			"temp_1": "37.0",
-			"Tctl":   "39.6",
+		},
+	}
+
+	c := NewCPUTemp(entity.Meta{"celsius": true})
+
+	res, err := c.process(input)
+	require.NoError(t, err)
+	require.EqualValues(t, output, res)
+}
+
+func TestCPUTemp_AMD(t *testing.T) {
+	input := `
+        k10temp-pci-00c3
+		Adapter: PCI adapter
+		Tctl:         +39.6°C	
+
+		acpitz-acpi-0
+		Adapter: ACPI interface
+		temp1:        +27.8°C  (crit = +119.0°C)
+	`
+	output := &entity.Payload{
+		State: "39.6",
+		Attributes: map[string]interface{}{
+			"temp_1": "27.8",
 		},
 	}
 
