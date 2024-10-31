342ec508dd8a8cccce8624cd395a23f67a2f1681
Paul Eggleton <bluelightning@bluelightning.org>
2024-10-31 23:11:35 +1300

    Add support for clearing notifications
    As per [1], tags and a notification message of "clear_notification" can
    be used to clear a previous notification, which is useful when a
    notification is about some warning state that has itself cleared. It
    seems that notify-send can't actually clear notifications, but
    fortunately we can use a simple call to gdbus instead. Handles clearing
    multiple notifications with the same tag.
    
    Tested with KDE Plasma 5.27.11.
    
    [1] https://companion.home-assistant.io/docs/notifications/notifications-basic/#clearing

--- notifications.go
+++ notifications.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os/exec"
 	"strconv"
+	"strings"
 	"time"
 
 	"hacompanion/api"
@@ -20,6 +21,7 @@ type NotificationServer struct {
 	mux          *http.ServeMux
 	address      string
 	Server       *http.Server
+	tags         map[string][]string
 }
 
 func NewNotificationServer(registration api.Registration, address string) *NotificationServer {
@@ -37,6 +39,7 @@ func NewNotificationServer(registration api.Registration, address string) *Notif
 		ReadTimeout:       time.Duration(20) * time.Second,
 		WriteTimeout:      time.Duration(20) * time.Second,
 	}
+	s.tags = make(map[string][]string)
 	return s
 }
 
@@ -56,13 +59,41 @@ func (s NotificationServer) Listen(_ context.Context) {
 			util.RespondError(w, "wrong token", http.StatusUnauthorized)
 			return
 		}
-		err = notification.Send(r.Context(), req.Title, req.Message, req.Data)
-		if err != nil {
-			log.Printf("failed to send notification: %s", err)
-			util.RespondError(w, err.Error(), http.StatusInternalServerError)
-			return
+		var id string
+		if req.Message == "clear_notification" {
+			idlist := s.tags[req.Data.Tag]
+			if len(idlist) > 0 {
+				var anysuccess bool = false
+				for _, id = range idlist {
+					err = notification.Clear(r.Context(), id)
+					if err == nil {
+						anysuccess = true
+					} else {
+						log.Printf("failed to clear notification: %s", err)
+					}
+				}
+				if !anysuccess {
+					log.Println("clear_notification failed")
+					util.RespondError(w, "clear_notification failed", http.StatusInternalServerError)
+					return
+				}
+				delete(s.tags, req.Data.Tag)
+			} else {
+				log.Printf("No notification found with tag %s", req.Data.Tag)
+			}
+			log.Println("notification sent successfully")
+		} else {
+			err, id = notification.Send(r.Context(), req.Title, req.Message, req.Data)
+			if err != nil {
+				log.Printf("failed to send notification: %s", err)
+				util.RespondError(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if req.Data.Tag != "" {
+				s.tags[req.Data.Tag] = append([]string{id}, s.tags[req.Data.Tag]...)
+			}
+			log.Println("notification cleared successfully")
 		}
-		log.Println("notification sent successfully")
 		w.WriteHeader(http.StatusCreated)
 		util.RespondSuccess(w)
 	})
@@ -77,7 +108,7 @@ func (s NotificationServer) Listen(_ context.Context) {
 // Notification is used to send notifications using native tools.
 type Notification struct{}
 
-func (n *Notification) Send(ctx context.Context, title, message string, data api.PushNotificationData) error {
+func (n *Notification) Send(ctx context.Context, title, message string, data api.PushNotificationData) (error, string) {
 	var args []string
 	if data.Expire > 0 {
 		args = append(args, "-t", strconv.Itoa(data.Expire))
@@ -91,8 +122,25 @@ func (n *Notification) Send(ctx context.Context, title, message string, data api
 	}
 	args = append(args, "-h", "string:desktop-entry:hacompanion")
 	args = append(args, message)
+	args = append(args, "-p")
 	log.Printf("comand is: notify-send %v", args)
 	cmd := exec.CommandContext(ctx, "notify-send", args...)
+	cmd.Env = []string{"DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus"}
+	out, err := cmd.Output()
+	if err != nil {
+		log.Printf("Return :%+v", err)
+		return err, ""
+	}
+	id := strings.TrimSpace(string(out[:]))
+	return nil, id
+}
+
+func (n *Notification) Clear(ctx context.Context, id string) error {
+	var args []string
+	args = append(args, "call", "--session", "--dest", "org.freedesktop.Notifications", "--object-path", "/org/freedesktop/Notifications", "--method",  "org.freedesktop.Notifications.CloseNotification")
+	args = append(args, id)
+	log.Printf("comand is: gdbus %v", args)
+	cmd := exec.CommandContext(ctx, "gdbus", args...)
 	cmd.Env = []string{"DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus"}
 	if err := cmd.Run(); err != nil {
 		log.Printf("Return :%+v", err)
