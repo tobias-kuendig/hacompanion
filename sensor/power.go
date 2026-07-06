73ecfd60a78fc0d5d74f729ca7f09983170e14ad
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2026-07-06 22:06:22 +0200

    feat(power): add macOS power sensor

--- sensor/power.go
+++ sensor/power.go
@@ -1,3 +1,5 @@
+//go:build !darwin
+
 package sensor
 
 import (
@@ -6,16 +8,11 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"hacompanion/entity"
 	"hacompanion/util"
 )
-
-type Power struct {
-	Battery string
-}
 
 func NewPower(m entity.Meta) *Power {
 	c := &Power{Battery: "BAT0"}
@@ -95,44 +92,4 @@ func (pwr Power) optimisticRead(file string) string {
 	}
 
 	return string(b)
-}
-
-func (pwr Power) resolveIcon(state any, charging bool) string {
-	num, err := strconv.Atoi(strings.TrimSpace(state.(string)))
-	if err != nil {
-		return "mdi:battery-unknown"
-	}
-
-	// Normalize to MDI steps
-	var level int
-	switch {
-	case num >= 90:
-		level = 100
-	case num >= 80:
-		level = 80
-	case num >= 70:
-		level = 70
-	case num >= 60:
-		level = 60
-	case num >= 50:
-		level = 50
-	case num >= 40:
-		level = 40
-	case num >= 30:
-		level = 30
-	case num >= 20:
-		level = 20
-	default:
-		level = 10
-	}
-
-	if level == 100 {
-		return "mdi:battery"
-	}
-
-	if charging {
-		return fmt.Sprintf("mdi:battery-charging-%d", level)
-	}
-
-	return fmt.Sprintf("mdi:battery-%d", level)
 }
